f07892d57d75c36b782e5dc4fde138bbf414c4c2
hassan <hassanhanif227@gmail.com>
2024-08-11 05:59:46 -0700

    any

--- university.py/hello.py
+++ university.py/hello.py
@@ -1,1 +1,7 @@
-print("pakistan")
+my_list=1,2,3,4,4,3,2,2
+lst={}
+
+for i in my_list:
+     lst[i]=lst.get(i,0)+1
+
+print(lst)
--- university.py/salam.py
+++ university.py/salam.py
@@ -1,1 +1,2 @@
-print("pakistan zindabad")
+for i in range(1,10):
+ print(i)
